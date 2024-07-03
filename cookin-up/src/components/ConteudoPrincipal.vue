74a57110638f3aad2193c659184c77a3f06bb5c7
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2024-07-03 16:29:16 -0300

    Create ingredient list with v-if/v-else for empty state

--- cookin-up/src/components/ConteudoPrincipal.vue
+++ cookin-up/src/components/ConteudoPrincipal.vue
@@ -15,14 +15,19 @@
     <section>
       <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
 
-      <ul class="ingredientes-sua-lista">
+      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
         <li
-          v-for="ingrediente in ingredientes"
+          v-for="ingrediente in ingredientes" :key="ingrediente"
           class="ingrediente"
         >
           {{ ingrediente }}
         </li>
       </ul>
+
+      <p v-else class="paragrafo lista-vazia">
+        <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+        Sua lista está vazia, selecione ingredientes para para iniciar.
+      </p>
     </section>
   </main>
 </template>
