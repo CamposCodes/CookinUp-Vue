91a2b6381c4fe93ebe594a956143aa1357e7faf6
CamposCodes <campos.gabriel@estudante.ufjf.br>
2025-08-18 11:43:47 -0300

    feat: ajuste lógica de filtro de receitas + ajuste manual em CardCategoria

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -18,13 +18,13 @@ export default {
     <article class="categoria">
       <header class="categoria__cabecalho">
         <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" :alt="`Ícone de ${categoria.nome}`" class="categoria__imagem">
-  
+
         <h2 class="paragrafo-lg categoria__nome">{{ categoria.nome }}</h2>
       </header>
-  
+
       <ul class="categoria__ingredientes">
         <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-          <ingredienteSelecionavel :ingrediente="ingrediente" 
+          <ingredienteSelecionavel :ingrediente="ingrediente"
           @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
           @remover-ingrediente="$emit('removerIngrediente', $event)"
           />
@@ -41,30 +41,30 @@ export default {
     background: var(--branco, #FFF);
     box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
     height: 100%;
-  
+
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 2rem;
   }
-  
+
   .categoria__cabecalho {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 0.5rem;
   }
-  
+
   .categoria__imagem {
     width: 3.5rem;
   }
-  
+
   .categoria__nome {
     text-align: center;
     color: var(--verde-medio, #3D6D4A);
     font-weight: 700;
   }
-  
+
   .categoria__ingredientes {
     display: flex;
     justify-content: center;
